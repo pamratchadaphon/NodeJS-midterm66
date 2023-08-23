d5c4e7c0e6a3c1c929bd12ff2b9d2761213bee55
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2023-08-23 13:55:09 +0700

    api2

--- server/src/controller/UserController.js
+++ server/src/controller/UserController.js
@@ -3,8 +3,15 @@ module.exports={
         const num1 = req.body
         const sum = (num1.num)+1
         res.send("ค่าคือ "+sum)
+    },
+    que22(req,res){
+        const num1 = req.body
+        const num11 = num1.num
+        
+        if (((num11 % 2 == 0) || (num11 % 3 == 0) || (num11 % 5 == 0) || (num11 % 7 == 0))&& (num11!=2 || num11!=3 || num11!=5||num11!=7)) {
+            res.send("เป็นจำนวนเฉพาะ")
+        }else{
+           res.send("ไม่เป็นจำนวนเฉพาะ")
+        }
     }
-    // que22(req,res){
-
-    // }
 }
--- server/src/route.js
+++ server/src/route.js
@@ -2,6 +2,5 @@ const UserController = require('./controller/UserController')
 
 module.exports=(app)=>{
     app.post('/que1',UserController.que11)
-    // app.post('/que2',UserController.que22)
-    // app.get('/que1/:num',UserController.que13)
+    app.post('/que2',UserController.que22)
 }
